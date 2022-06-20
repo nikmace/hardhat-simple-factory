63ce15c46bf1b04c40cb24a4f61c3f3568f0d2fe
nikmace <nikmacentosh@gmail.com>
2022-06-20 17:31:32 +0300

    Script to deploy our contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,7 +3,7 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { ethers, run, network } from 'hardhat';
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -14,17 +14,53 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const SimpleStorageFC = await ethers.getContractFactory('SimpleStorage');
+  console.log('Deploying contract ...');
+  const contract = await SimpleStorageFC.deploy();
 
-  await greeter.deployed();
+  await contract.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log('Contract deployed to:', contract.address);
+
+  // Check what Network we have
+  if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+    await contract.deployTransaction.wait(6);
+    // Verify our contract
+    await verify(contract.address, []);
+  }
+
+  const currentVal = await contract.retrieve();
+  console.log(`Current value: ${currentVal}`);
+
+  const txResponse = await contract.store(32);
+  await txResponse.wait(1);
+  console.log(`Updated value to ${await contract.retrieve()}`);
+}
+
+async function verify(contractAdrress: string, args: []) {
+  console.log('Verifying contract ...');
+  try {
+    await run('verify:verify', {
+      address: contractAdrress,
+      constructorArguments: args,
+    });
+  } catch (e: any) {
+    if (e.message.toLowerCase().includes('already verified')) {
+      console.log('Already verified!');
+    } else {
+      console.log(e);
+    }
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => {
+    console.log('Successfully deployed');
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
